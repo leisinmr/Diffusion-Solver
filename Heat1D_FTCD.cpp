8f03de83a5e339bfa9c0966fe7a424d484e2a380
Matt Leisinger <leisinmr@gmail.com>
2012-02-11 05:42:57 -0600

    1D Heat Diffusion working\n Added a file stream object for writing CSV output

--- Heat1D_FTCD.cpp
+++ Heat1D_FTCD.cpp
@@ -6,42 +6,49 @@
 
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
-
-class FileWriter
+class FileStream
 { 
     public:
-        FileWriter(std::string name, char delimiter = ',') 
-            : m_name(name), m_delim(delimiter)
+        FileStream(std::string name, char delimiter = ',') 
+            : m_name(name), m_delim(delimiter), m_stream(name.c_str())
         {
         }
 
-        ~FileWriter()
+        ~FileStream()
         {
-            // TODO
+            m_stream.close();
         }
 
-        bool open() 
+        void writeline(double *row, int size)
         {
-            m_handle = fopen(m_name.c_str(), "w");
-        }
-
-        void writeline(std::string line)
-        {
-            // TODO
+            for (int i = 0; i < size; ++i)
+            {
+                m_stream << row[i] << ",";
+                //m_stream << (i != size-1) ? "," : "";
+            }
+            m_stream << '\n';
         }
 
         // write any data type converable to a string
         template <typename T>
         void writeline(T items, size_t num)
         {
+
         }
 
     private:
         std::string m_name;
         char m_delim;
-        FILE *m_handle;
+        std::ofstream m_stream;
 };
+
+/*template <typename stream>
+stream& operator<<(const stream& s, double val)
+{
+    stream << val;
+}*/
 
 double* createLinspace(int start, int end, int numIntervals)
 {
@@ -81,12 +88,18 @@ int main(int argc, char** argv)
     int L = 1;
     double alpha = 0.001;
 
-    double dx = L/nx;
+    double dx = (double)L/nx;
     double dt = 0.0005;
 
     int nsteps = 1000;
 
     double C = alpha*dt/pow(dx,2);
+
+    std::cout << "Starting 1D Heat Solver with: " << std::endl;
+    std::cout << "nx = " << nx << ", alpha= " << alpha;
+    std::cout << ", dx= " << dx << ", dt= " << dt << std::endl;
+
+    std::cout << "C = " << C << std::endl;
 
     double *x = createLinspace(0, L, nx+2);
 
@@ -105,11 +118,26 @@ int main(int argc, char** argv)
         exit(2);
     }
 
+    FileStream f("Heat1D.csv");
     for (int n = 0; n < nsteps; ++n)
     {
+        // Zero at the boundary conditions
+        Tnew[0] = 0; Tnew[nx+1] = 0;
+        // solve for one temperature step
         for (int i = 2; i < nx+1; ++i)
         {
+            // apply the FTCD across the 1D rod
             Tnew[i] = T[i] + C*(T[i-1] - 2*T[i] + T[i+1]);
+
+        }
+
+        // write out to a file
+        f.writeline(Tnew, nx+2);
+        
+        for (int i = 2; i < nx+1; ++i)
+        {
+            // set Tnew to T so we can do the next step
+            T[i] = Tnew[i];
         }
     }
 
